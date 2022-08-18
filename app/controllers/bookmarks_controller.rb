0568b5ae9937af063f0a19a39914884724777374
Caroline Brum <carolinefbrum@gmail.com>
2022-08-18 17:05:35 -0300

    in progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,40 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[destroy show]
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show; end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmark_path(@bookmark), status: :see_other
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
